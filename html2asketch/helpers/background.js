9ad1c15dcf7ff8d1d14bbd05d6c5fe665ad9a28f
kimdalhyeong <5gockcook@naver.com>
2017-12-18 07:27:57 +0900

    Add handling for image request failures

--- html2asketch/helpers/background.js
+++ html2asketch/helpers/background.js
@@ -97,136 +97,140 @@ const parseLinearGradient = value => {
   return null;
 };
 
-async function fixBackgroundImage(url, width, height, backgroundSize, backgroundPosition, backgroundRepeat) {
+async function toImageObj(url) {
   const imageObj = new Image();
 
-  imageObj.setAttribute('crossOrigin', 'anonymous');
-  await new Promise(resolve => {
+  imageObj.crossOrigin = 'anonymous';
+  await new Promise((resolve, reject) => {
     imageObj.src = url;
     imageObj.onload = resolve;
+    imageObj.onerror = reject;
   });
 
-  const imageW = imageObj.naturalWidth;
-  const imageH = imageObj.naturalHeight;
-  let x, y, w, h;
+  return imageObj;
+}
 
-  if (backgroundSize === 'cover' && imageW >= imageH || backgroundSize === 'contain' && imageW < imageH) {
-    w = imageW * height / imageH;
-    h = height;
-  } else if (backgroundSize === 'cover' && imageW < imageH || backgroundSize === 'contain' && imageW >= imageH) {
-    w = width;
-    h = imageH * width / imageW;
-  } else if (backgroundSize === 'auto') {
-    w = imageW;
-    h = imageH;
-  } else {
-    const backgroundSizeMatches = backgroundSize.match(/([-]?[0-9.]+)([^ ]+)? ?(?:([-]?[0-9.]+)([^ ])?)?/i);
+async function fixBackgroundImage(url, width, height, backgroundSize, backgroundPosition, backgroundRepeat) {
+  let imageObj = await toImageObj(url).catch(e => {console.error(e);});
+  let imageData = url;
 
-    if (backgroundSizeMatches) {
-      switch (backgroundSizeMatches[2]) {
+  if (imageObj) {
+    const imageW = imageObj.naturalWidth;
+    const imageH = imageObj.naturalHeight;
+    let x, y, w, h;
+
+    if (backgroundSize === 'cover' && imageW >= imageH || backgroundSize === 'contain' && imageW < imageH) {
+      w = imageW * height / imageH;
+      h = height;
+    } else if (backgroundSize === 'cover' && imageW < imageH || backgroundSize === 'contain' && imageW >= imageH) {
+      w = width;
+      h = imageH * width / imageW;
+    } else if (backgroundSize === 'auto') {
+      w = imageW;
+      h = imageH;
+    } else {
+      const backgroundSizeMatches = backgroundSize.match(/([-]?[0-9.]+)([^ ]+)? ?(?:([-]?[0-9.]+)([^ ])?)?/i);
+
+      if (backgroundSizeMatches) {
+        switch (backgroundSizeMatches[2]) {
+          case 'px':
+            w = backgroundSizeMatches[1];
+            break;
+          case '%':
+            w = width / 100 * backgroundSizeMatches[1];
+            break;
+          default:
+            w = 0;
+        }
+        switch (backgroundSizeMatches[4]) {
+          case 'px':
+            h = backgroundSizeMatches[3];
+            break;
+          case '%':
+            h = height / 100 * backgroundSizeMatches[3];
+            break;
+          default:
+            h = imageH * w / imageW;
+        }
+      }
+    }
+
+    const backgroundPositionMatches = backgroundPosition.match(/([-]?[0-9.]+)([^ ]+)? ([-]?[0-9.]+)([^ ]+)?/i);
+
+    if (backgroundPositionMatches) {
+      switch (backgroundPositionMatches[2]) {
         case 'px':
-          w = backgroundSizeMatches[1];
+          x = backgroundPositionMatches[1];
           break;
         case '%':
-          w = width / 100 * backgroundSizeMatches[1];
+          x = (width - w) / 100 * backgroundPositionMatches[1];
           break;
         default:
-          w = 0;
+          x = 0;
       }
-      switch (backgroundSizeMatches[4]) {
+      switch (backgroundPositionMatches[4]) {
         case 'px':
-          h = backgroundSizeMatches[3];
+          y = backgroundPositionMatches[3];
           break;
         case '%':
-          h = height / 100 * backgroundSizeMatches[3];
+          y = (height - h) / 100 * backgroundPositionMatches[3];
           break;
         default:
-          h = imageH * w / imageW;
+          y = 0;
       }
     }
-  }
 
-  const backgroundPositionMatches = backgroundPosition.match(/([-]?[0-9.]+)([^ ]+)? ([-]?[0-9.]+)([^ ]+)?/i);
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
 
-  if (backgroundPositionMatches) {
-    switch (backgroundPositionMatches[2]) {
-      case 'px':
-        x = backgroundPositionMatches[1];
-        break;
-      case '%':
-        x = (width - w) / 100 * backgroundPositionMatches[1];
-        break;
-      default:
-        x = 0;
+    if (backgroundRepeat !== 'no-repeat') {
+      // draw patternSource;
+      canvas.width = w;
+      canvas.height = h;
+      context.drawImage(imageObj, 0, 0, imageW, imageH, 0, 0, w, h);
+      imageObj = await toImageObj(canvas.toDataURL('image/png')).catch(e => {console.error(e);});
+
+      // draw pattern;
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      const patternWidth = x >= 0 ? width + x - Math.ceil(x / w) * w : width - x;
+      const patternHeight = y >= 0 ? height + y - Math.ceil(y / h) * h : height - y;
+
+      canvas.width = patternWidth;
+      canvas.height = patternHeight;
+      const pattern = context.createPattern(imageObj, backgroundRepeat);
+
+      context.fillStyle = pattern;
+      context.fillRect(0, 0, patternWidth, patternHeight);
+
+      imageObj = await toImageObj(canvas.toDataURL('image/png')).catch(e => {console.error(e);});
+
+      // draw background image;
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      canvas.width = width;
+      canvas.height = height;
+      const patternX = x >= 0 && backgroundRepeat !== 'repeat-y' ? x - Math.ceil(x / w) * w : x;
+      const patternY = y >= 0 && backgroundRepeat !== 'repeat-x' ? y - Math.ceil(y / h) * h : y;
+
+      context.drawImage(
+        imageObj,
+        0,
+        0,
+        imageObj.naturalWidth,
+        imageObj.naturalHeight,
+        patternX,
+        patternY,
+        imageObj.naturalWidth,
+        imageObj.naturalHeight
+      );
+    } else {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(imageObj, 0, 0, imageW, imageH, x, y, w, h);
     }
-    switch (backgroundPositionMatches[4]) {
-      case 'px':
-        y = backgroundPositionMatches[3];
-        break;
-      case '%':
-        y = (height - h) / 100 * backgroundPositionMatches[3];
-        break;
-      default:
-        y = 0;
-    }
+    imageData = canvas.toDataURL('image/png');
   }
 
-  const canvas = document.createElement('canvas');
-  const context = canvas.getContext('2d');
-
-  if (backgroundRepeat !== 'no-repeat') {
-    // draw patternSource;
-    canvas.width = w;
-    canvas.height = h;
-    context.drawImage(imageObj, 0, 0, imageW, imageH, 0, 0, w, h);
-
-    await new Promise(resolve => {
-      imageObj.src = canvas.toDataURL('image/png');
-      imageObj.onload = resolve;
-    });
-
-    // draw pattern;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    const patternWidth = x >= 0 ? width + x - Math.ceil(x / w) * w : width - x;
-    const patternHeight = y >= 0 ? height + y - Math.ceil(y / h) * h : height - y;
-
-    canvas.width = patternWidth;
-    canvas.height = patternHeight;
-    const pattern = context.createPattern(imageObj, backgroundRepeat);
-
-    context.fillStyle = pattern;
-    context.fillRect(0, 0, patternWidth, patternHeight);
-
-    await new Promise(resolve => {
-      imageObj.src = canvas.toDataURL('image/png');
-      imageObj.onload = resolve;
-    });
-
-    // draw background image;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    canvas.width = width;
-    canvas.height = height;
-    const patternX = x >= 0 && backgroundRepeat !== 'repeat-y' ? x - Math.ceil(x / w) * w : x;
-    const patternY = y >= 0 && backgroundRepeat !== 'repeat-x' ? y - Math.ceil(y / h) * h : y;
-
-    context.drawImage(
-      imageObj,
-      0,
-      0,
-      imageObj.naturalWidth,
-      imageObj.naturalHeight,
-      patternX,
-      patternY,
-      imageObj.naturalWidth,
-      imageObj.naturalHeight
-    );
-  } else {
-    canvas.width = width;
-    canvas.height = height;
-    context.drawImage(imageObj, 0, 0, imageW, imageH, x, y, w, h);
-  }
-
-  return canvas.toDataURL('image/png');
+  return imageData;
 }
 
 export {
