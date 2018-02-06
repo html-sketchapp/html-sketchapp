74f56a720ae4d6629aa303b6b1cdb9b8e5daa5d1
Thibaut <thibaut@thibaut.re>
2018-02-06 15:45:22 +0100

    Fix line-through text decoration (#57)
    * :bug: Fix line-through compted style
    
    * :bug: Use textDecorationLine
    
    On Chrome, textDecoration contains the line, the color and the style
    Sketch seems to only support the line attribute (line-through or
    underline)

--- html2asketch/nodeToSketchLayers.js
+++ html2asketch/nodeToSketchLayers.js
@@ -104,7 +104,7 @@ export default async function nodeToSketchLayers(node) {
     letterSpacing,
     color,
     textTransform,
-    textDecorationStyle,
+    textDecorationLine,
     textAlign,
     justifyContent,
     display,
@@ -231,7 +231,7 @@ export default async function nodeToSketchLayers(node) {
     fontWeight: parseInt(fontWeight, 10),
     color,
     textTransform,
-    textDecoration: textDecorationStyle,
+    textDecoration: textDecorationLine,
     textAlign: display === 'flex' || display === 'inline-flex' ? justifyContent : textAlign
   });
 
