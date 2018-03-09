4176cd4bca2a1e80c2e4cc70d34903fbd1a93a45
Vladimir Starkov <iamstarkov@gmail.com>
2018-03-09 11:06:53 +0100

    build sketch plugin

--- asketch2sketch.sketchplugin/Contents/Sketch/asketch2sketch.js
+++ asketch2sketch.sketchplugin/Contents/Sketch/asketch2sketch.js
@@ -816,12 +816,10 @@ module.exports.SET_SCRIPT_RESULT = 'playground/SET_SCRIPT_RESULT'
 
 "use strict";
 /**
- * Copyright 2013-2015, Facebook, Inc.
- * All rights reserved.
+ * Copyright (c) 2013-present, Facebook, Inc.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 
