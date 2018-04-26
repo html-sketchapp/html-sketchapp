d69e05a0ae2015241e1f903027dbf0c05934e744
Konrad Dzwinel <kdzwinel@gmail.com>
2018-04-26 13:36:32 +0200

    Update type definition

--- typings/model/base.d.ts
+++ typings/model/base.d.ts
@@ -4,6 +4,7 @@ export default class Base {
   public addLayer(layer: Base): void;
   public getID(): string;
   public setFixedWidthAndHeight(): void;
+  public setResizingConstraint(...constraints: string[]): void;
   public setName(name: string): void;
   public setStyle(style: any): void;
   public toJSON(): string;
