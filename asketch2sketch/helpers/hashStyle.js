266851e23a47eb7afe41e16254c9c0fd26467007
Konrad Dzwinel <kdzwinel@gmail.com>
2018-05-14 22:15:11 +0200

    eslint fixes

--- asketch2sketch/helpers/hashStyle.js
+++ asketch2sketch/helpers/hashStyle.js
@@ -1,6 +1,6 @@
 import murmurHash from 'murmur2js';
 import sortObjectKeys from './sortObjectKeys';
 
-const hashStyle = obj => obj ? murmurHash(JSON.stringify(sortObjectKeys(obj))) : 0;
-
-export default hashStyle;
+export default function hashStyle(obj) {
+  return obj ? murmurHash(JSON.stringify(sortObjectKeys(obj))) : 0;
+}
