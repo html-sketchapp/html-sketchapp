baf0d51db45e9fb84ec3f73c35056de0a9c2abfc
Konrad Dzwinel <kdzwinel@gmail.com>
2018-05-14 22:04:44 +0200

    Fix text color issue in Sketch 50. Use Sketch UI module to show dialogs and success messages. Update react-sketchapp copy&paste dependencies.

--- asketch2sketch/asketch2sketch.js
+++ asketch2sketch/asketch2sketch.js
@@ -1,8 +1,8 @@
+import UI from 'sketch/ui';
 import {fromSJSONDictionary} from 'sketchapp-json-plugin';
 import {fixTextLayer, fixSharedTextStyle} from './helpers/fixFont';
 import fixImageFill from './helpers/fixImageFill';
 import fixSVGLayer from './helpers/fixSVG';
-import showDialog from './helpers/showDialog';
 import zoomToFit from './helpers/zoomToFit';
 
 function removeExistingLayers(context) {
@@ -157,9 +157,13 @@ export default function asketch2sketch(context) {
       .forEach(layer => layer && page.addLayer(layer));
 
     if (failingLayers.length === 1) {
-      showDialog('One layer couldn\'t be imported and was skipped.');
+      UI.alert('asketch2sketch', 'One layer couldn\'t be imported and was skipped.');
     } else if (failingLayers.length > 1) {
-      showDialog(`${failingLayers.length} layers couldn't be imported and were skipped.`);
+      UI.alert('asketch2sketch', `${failingLayers.length} layers couldn't be imported and were skipped.`);
+    } else {
+      const emojis = ['👌', '👍', '✨', '😍', '🍾', '🤩', '🎉', '👏', '💪', '🤘', '💅', '🏆', '🚀'];
+
+      UI.message(`Import successful ${emojis[Math.floor(emojis.length * Math.random())]}`);
     }
 
     zoomToFit(context);
--- asketch2sketch/helpers/hashStyle.js
+++ asketch2sketch/helpers/hashStyle.js
@@ -1,6 +1,6 @@
 import murmurHash from 'murmur2js';
 import sortObjectKeys from './sortObjectKeys';
 
-const hashStyle = obj => murmurHash(JSON.stringify(sortObjectKeys(obj)));
+const hashStyle = obj => obj ? murmurHash(JSON.stringify(sortObjectKeys(obj))) : 0;
 
 export default hashStyle;
