067da72ca53163e6a2030e0a15ef309df0193eb5
patrycjaradaczynska <patrycja.radaczynska@brainly.com>
2019-02-18 21:25:26 +0100

    Small adjustments in methods

--- asketch2sketch/asketch2sketch.js
+++ asketch2sketch/asketch2sketch.js
@@ -89,14 +89,14 @@ function addSharedTextStyle(document, style) {
 function removeSharedColors(document) {
   const assets = document.documentData().assets();
 
-  assets.removeAllColors();
+  assets.removeAllColorAssets();
 }
 
 function addSharedColor(document, colorJSON) {
   const assets = document.documentData().assets();
   const color = fromSJSONDictionary(colorJSON);
 
-  assets.addColor(color);
+  assets.addAsset(color);
 }
 
 export default function asketch2sketch(context, asketchFiles) {
