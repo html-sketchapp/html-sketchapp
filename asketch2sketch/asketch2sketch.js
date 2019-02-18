263fbc62fc36d3c14e3c56c389b95415e5d0aa81
patrycjaradaczynska <patrycja.radaczynska@brainly.com>
2019-02-18 21:32:32 +0100

    Change to addColorAsset

--- asketch2sketch/asketch2sketch.js
+++ asketch2sketch/asketch2sketch.js
@@ -96,7 +96,7 @@ function addSharedColor(document, colorJSON) {
   const assets = document.documentData().assets();
   const color = fromSJSONDictionary(colorJSON);
 
-  assets.addAsset(color);
+  assets.addColorAsset(color);
 }
 
 export default function asketch2sketch(context, asketchFiles) {
