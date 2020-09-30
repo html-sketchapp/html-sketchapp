f8947f48e52769b0c97b749df3dc12af3da64669
Piotr Hebda <tlenex@users.noreply.github.com>
2020-09-30 17:36:23 +0200

    Update convertAngleToFromAndTo.js
    Support other angle units

--- html2asketch/helpers/convertAngleToFromAndTo.js
+++ html2asketch/helpers/convertAngleToFromAndTo.js
@@ -1,17 +1,42 @@
 // Keep this pure for easy testing in the future.
 export default function convertAngleToFromAndTo(angle) {
-  function convertAngleInDegreesToFromAndTo(degrees) {
-    // we begin "from" (0,0) and have a circle (returned as "to") with starting from point at (1,0) for 0deg (to right)
-    // default 0deg
-    const from = {x: 0, y: 0};
-    const to = {x: 1, y: 0};
+  function parseAngleToRadians(angle) {
+    const match = angle.match(/^(-?(?:\d+)?(?:\.\d+)?)(deg|grad|rad|turn)$/);
 
-    if (degrees === 0) {
+    if (!match) {
+      return Number.NaN;
+    }
+
+    const value = parseFloat(match[1]) + ;
+    const unit = match[2];
+
+    switch (unit) {
+      case 'deg':
+        return value * (Math.PI / 180);
+      case 'grad':
+        return value * (Math.PI / 200);
+      case 'rad':
+        return value;
+      case 'turn':
+        return value * 2 * Math.PI;
+      default:
+        return Number.NaN;
+    }
+  }
+  
+  function convertRadiansToFromAndTo(radians) {
+    // we begin "from" (0,0) and have a circle (returned as "to") with starting from point at (0,-1) for 0deg (to bottom)
+    // default 0deg
+    const from = { x: 0, y: 0 };
+    const to = { x: 0, y: -1 };
+
+    if (radians === 0) {
       return {from, to};
     }
 
     // sin() and cos() needs radians to work with
-    const angleInRadians = degrees * (Math.PI / 180);
+    // and we need to rotate the whole thing by 90deg so we support "to bottom" as default
+    const angleInRadians = radians + parseAngleToRadians('-270deg');
 
     // calculate circle's (x,y) wich is sin nad cos of given radians
     // we support precision to make things human readable
@@ -25,28 +50,36 @@ export default function convertAngleToFromAndTo(angle) {
 
     return {
       from,
-      to: {x, y},
+      to: { x, y },
     };
   }
 
   // we need angle to be a float number
-  const degrees = Number.parseFloat(angle);
+  const radians = parseAngleToRadians(angle);
 
   // but if we don't have a number we also support some strings
-  if (Number.isNaN(degrees)) {
-    switch (angle) {
-      case 'to right':
-        return convertAngleInDegreesToFromAndTo(0);
+  if (Number.isNaN(radians)) {
+    switch (angle.toLowerCase()) {
       case 'to bottom':
-        return convertAngleInDegreesToFromAndTo(90);
-      case 'to left':
-        return convertAngleInDegreesToFromAndTo(180);
+        return convertRadiansToFromAndTo(parseAngleToRadians('0deg'));
+      case 'to right bottom':
+        return convertRadiansToFromAndTo(parseAngleToRadians('45deg'));
+      case 'to right':
+        return convertRadiansToFromAndTo(parseAngleToRadians('90deg'));
+      case 'to right top':
+        return convertRadiansToFromAndTo(parseAngleToRadians('135deg'));
       case 'to top':
-        return convertAngleInDegreesToFromAndTo(270);
+        return convertRadiansToFromAndTo(parseAngleToRadians('180deg'));
+      case 'to left top':
+        return convertRadiansToFromAndTo(parseAngleToRadians('225deg'));
+      case 'to right':
+        return convertRadiansToFromAndTo(parseAngleToRadians('270deg'));
+      case 'to left bottom':
+        return convertRadiansToFromAndTo(parseAngleToRadians('315deg'));
       default:
         throw TypeError('Incorrect angle.');
     }
   }
 
-  return convertAngleInDegreesToFromAndTo(degrees);
+  return convertRadiansToFromAndTo(radians);
 }
