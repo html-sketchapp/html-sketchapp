07aa5ecde5777b741c494f924dab3cf4eb3a08cb
Mike Skowronek <skowronkow@gmail.com>
2021-01-12 16:54:42 +0100

    Change closest matching font algorithm (#212)
    * Use map range to compare matching font
    
    * Apply new range based algorithm to find closes matching font
    
    * Remove debug log
    
    * Remove empty line
    
    * Fix comment
    
    * Fix comment

--- asketch2sketch/helpers/findFont.js
+++ asketch2sketch/helpers/findFont.js
@@ -28,6 +28,18 @@ const FONT_WEIGHTS = {
   '900': NSFontWeightBlack,
 };
 
+const WEIGHT_MAP = {
+  [NSFontWeightUltraLight]: [-1.0, -0.70],
+  [NSFontWeightThin]: [-0.70, -0.45],
+  [NSFontWeightLight]: [-0.45, -0.10],
+  [NSFontWeightRegular]: [-0.10, 0.10],
+  [NSFontWeightMedium]: [0.10, 0.27],
+  [NSFontWeightSemibold]: [0.27, 0.35],
+  [NSFontWeightBold]: [0.35, 0.50],
+  [NSFontWeightHeavy]: [0.50, 0.60],
+  [NSFontWeightBlack]: [0.60, 1.0],
+};
+
 const isItalicFont = font => {
   const traits = font.fontDescriptor().objectForKey(NSFontTraitsAttribute);
   const symbolicTraits = traits[NSFontSymbolicTrait].unsignedIntValue();
@@ -165,8 +177,12 @@ const findFont = style => {
     }
   }
 
-  // Get the closest font that matches the given weight for the fontFamily
-  let closestWeight = Infinity;
+  // Get the font that matches the given weight for the fontFamily.
+  // We don't break the loop even if we find matching element because
+  // the heaviest matching element should win.
+  // E.g. If weight of extra bold font found in the system is 0.61 and would match NSFontWeightBlack
+  // and there is another heavier font - black, with 0.8 weight, the latter will be picked.
+  const fontWeightRange = WEIGHT_MAP[fontWeight];
 
   for (let i = 0; i < fontNames.length; i += 1) {
     const match = NSFont.fontWithName_size(fontNames[i], fontSize);
@@ -174,10 +190,8 @@ const findFont = style => {
     if (isItalic === isItalicFont(match) && isCondensed === isCondensedFont(match)) {
       const testWeight = weightOfFont(match);
 
-      if (Math.abs(testWeight - fontWeight) < Math.abs(closestWeight - fontWeight)) {
+      if (testWeight >= fontWeightRange[0] && testWeight < fontWeightRange[1]) {
         font = match;
-
-        closestWeight = testWeight;
       }
     }
   }
